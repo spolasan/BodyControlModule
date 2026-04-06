db40e698f2b97ddb78178e744bfaa3521662fb57
spolasan <saikrishnaraopolasani@gmail.com>
2026-04-06 12:50:11 -0400

    Update run_mil.m

--- TurnLights_System/MIL_Test_Files/Manual_Test/run_mil.m
+++ TurnLights_System/MIL_Test_Files/Manual_Test/run_mil.m
@@ -1,12 +1,20 @@
 clc;
 clear;
 
-disp('Starting MIL test execution...');
+disp('=== Jenkins MIL Run Started ===');
 
-sltest.testmanager.clear;
-sltest.testmanager.load('TurnLights_System/MIL_Test_Files/Manual_Test/TurnLights_TestSuite.mldatx');
+try
+    sltest.testmanager.clear;
+    sltest.testmanager.load('TurnLights_System/MIL_Test_Files/Manual_Test/TurnLights_TestSuite.mldatx');
+    results = sltest.testmanager.run;
 
-results = sltest.testmanager.run;
+    disp('=== MIL Run Completed Successfully ===');
+    disp(results);
 
-disp('MIL test execution completed.');
-disp(results);
+catch ME
+    disp('=== MIL Run Failed ===');
+    disp(getReport(ME));
+    exit(1);
+end
+
+exit(0);
