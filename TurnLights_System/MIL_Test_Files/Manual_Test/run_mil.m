62c24dffa00da04b6e95a337ab66c8a3c8d970a3
spolasan <saikrishnaraopolasani@gmail.com>
2026-04-07 12:03:08 -0400

    Update run_mil.m

--- TurnLights_System/MIL_Test_Files/Manual_Test/run_mil.m
+++ TurnLights_System/MIL_Test_Files/Manual_Test/run_mil.m
@@ -1,6 +1,32 @@
 clc;
 clear;
-disp('RUN_MIL STARTED');
-pause(5);
-disp('RUN_MIL ENDED');
-exit(0);
+
+disp('=== Jenkins MIL Run Started ===');
+
+try
+    % Optional: close existing models/data from previous state
+    bdclose('all');
+
+    % Clear Test Manager
+    sltest.testmanager.clear;
+
+    % Load the test file
+    tf = 'TurnLights_System/MIL_Test_Files/Manual_Test/TurnLights_TestSuite.mldatx';
+    disp(['Loading Test File: ', tf]);
+    sltest.testmanager.load(tf);
+
+    % Run tests
+    disp('Running Test Manager suite...');
+    results = sltest.testmanager.run;
+
+    % Display results object
+    disp(results);
+
+    disp('=== MIL Run Completed Successfully ===');
+    exit(0);
+
+catch ME
+    disp('=== MIL Run Failed ===');
+    disp(getReport(ME, 'extended', 'hyperlinks', 'off'));
+    exit(1);
+end
